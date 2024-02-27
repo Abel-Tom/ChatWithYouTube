cc60e497155d075e836cfa2cdc038bc2ae7072ab
Abel Thomas <abel.thomas@teamwave.com>
2024-02-28 03:40:00 +0530

    renamed new ui file

--- main.py
+++ main.py
@@ -1,35 +1,36 @@
 import streamlit as st
 import langchain_helper as lch
-import textwrap
-
-st.title("YouTube Assistant")
 
 with st.sidebar:
-    with st.form(key='my_form'):
-        youtube_url = st.sidebar.text_area(
-            label="What is the YouTube video URL?",
-            max_chars=50
-            )
-        query = st.sidebar.text_area(
-            label="Ask me about the video?",
-            max_chars=100,
-            key="query"
-            )
-        openai_api_key = st.sidebar.text_input(
-            label="OpenAI API Key",
-            key="langchain_search_api_key_openai",
-            type="password"
-            )
-        "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-        submit_button = st.form_submit_button(label='Submit')
-if openai_api_key and openai_api_key.startswith('sk-'):
+    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+    url = st.text_input("Enter the YouTube video url", key="url", type="default")
+    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+    "[View the source code](https://github.com/TomHellCat/ChatWithYouTube)"
+
+st.title("💬 Chat with YouTube")
+st.caption("🚀 A streamlit chatbot powered by OpenAI LLM")
+if "messages" not in st.session_state:
+        st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+if prompt := st.chat_input():
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
+    if not url and not url.startswith('https://www.youtube.com/watch?v='):
+        st.info("Please enter a YouTube Video url to continue.")
+        st.stop()
+    if not url.startswith('https://www.youtube.com/watch?v='):
+        st.info("Please enter a valid YouTube Video url to continue.")
+        st.stop()
     assistant = lch.Assistant(openai_api_key)
-    if youtube_url:
-         db = assistant.create_db_from_youtube_video_url(youtube_url)
-         if query:
-            response, docs = assistant.get_response_from_query(db, query)
-            st.subheader("Answer:")
-            st.text(textwrap.fill(response, width=85))      
-else:
-    st.info("Please add your OpenAI API key to continue.")
-            
+    if url:
+         db = assistant.create_db_from_youtube_video_url(url)
+         if prompt:
+            st.session_state.messages.append({"role": "user", "content": prompt})
+            st.chat_message("user").write(prompt)
+            response, docs = assistant.get_response_from_query(db, prompt) 
+            st.session_state.messages.append({"role": "assistant", "content": response})
+            st.chat_message("assistant").write(response)
